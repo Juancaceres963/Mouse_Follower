b3a5bcef9b112d83103f329102a0b3dfb07a3815
Juancaceres963 <dnijuancaceres@gmail.com>
2023-06-02 17:21:30 -0300

    Mpuse Follower ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
-import './App.css'
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y:0 })
+
+  useEffect(() => {
+    console.log("efecto", {enabled});
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      console.log('handleMove',{clientX, clientY})
+      setPosition({ x: clientX, y: clientY })
+    }
+    if (enabled){
+      window.addEventListener('pointermove', handleMove)
+    } 
+    return (() => {
+      window.removeEventListener('pointermove', handleMove)
+    })
+  }, [enabled]);
 
   return (
-    <h1>Mouse Follower</h1>
-  )
+    <main>
+      <h1>Mouse Follower</h1>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -15,
+        top: -15,
+        width: 30,
+        height: 30,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
+  );
 }
 
-export default App
+export default App;
